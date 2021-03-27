e616cc654fef4f90209db2de848c2d9033232b98
Kyle Rockman <krockman@underarmour.com>
2021-03-27 12:46:01 -0500

    add zerolog

--- cmd/root.go
+++ cmd/root.go
@@ -2,14 +2,18 @@ package cmd
 
 import (
 	// "fmt"
-	// "os"
+	"os"
 	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var cfgFile string
+var logFormat string
+var logLevel string
 
 var rootCmd = &cobra.Command{
 	Use:   "kubectl-opslevel",
@@ -24,12 +28,15 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is ./opslevel.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default \"./opslevel.yaml\")")
+	rootCmd.PersistentFlags().StringVar(&logFormat, "logFormat", "JSON", "The Log Format. (options [\"JSON\", \"TEXT\"])")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "logLevel", "INFO", "The Log Level. (options [\"ERROR\", \"WARN\", \"INFO\", \"DEBUG\"])")
 	cobra.OnInitialize(initConfig)
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
+	setupLogging()
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
@@ -51,5 +58,23 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err != nil {
 		// TODO: log error
 		//fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func setupLogging() {
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	if logFormat == "TEXT" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+	if logLevel == "ERROR" {
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	} else if logLevel == "WARN" {
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	} else if logLevel == "DEBUG" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.16
 require (
 	github.com/creasty/defaults v1.5.1
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/rs/zerolog v1.21.0 // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/text v0.3.3 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
