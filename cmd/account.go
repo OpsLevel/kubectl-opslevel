84e5018af3934d582f3fe109371d4551a597dea4
Kyle Rockman <krockman@underarmour.com>
2021-05-08 12:00:15 -0500

    feature: Implement listing Team aliases available in your account

--- cmd/account.go
+++ cmd/account.go
@@ -43,12 +43,30 @@ var tierCmd = &cobra.Command{
 		}
 	},
 }
+
+var teamCmd = &cobra.Command{
+	Use:   "teams",
+	Short: "Lists the valid alias for teams in your account",
+	Long:  `Lists the valid alias for teams in your account`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := opslevel.NewClient(viper.GetString("apitoken"))
+		list, err := client.ListTeams()
+		if err == nil {
+			for _, item := range list {
+				fmt.Println(item.Alias)
+			}
+		}
+	},
+}
+
 func init() {
 	accountCmd.AddCommand(lifecycleCmd)
 	accountCmd.AddCommand(tierCmd)
+	accountCmd.AddCommand(teamCmd)
 	rootCmd.AddCommand(accountCmd)
 
 	// TODO: should this be a global flag?
 	lifecycleCmd.Flags().String("api-token", "", "The OpsLevel API Token. Overrides environment variable 'OL_APITOKEN'")
 	tierCmd.Flags().String("api-token", "", "The OpsLevel API Token. Overrides environment variable 'OL_APITOKEN'")
+	teamCmd.Flags().String("api-token", "", "The OpsLevel API Token. Overrides environment variable 'OL_APITOKEN'")
 }
