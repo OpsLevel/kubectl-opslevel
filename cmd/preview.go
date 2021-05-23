03d7ed9cdd34e75066d198d86509c1a3e5523270
Kyle Rockman <krockman@underarmour.com>
2021-05-23 13:35:16 -0500

    feature: improve the output that surrounds the `service preview` data to better instruct the user what to do next

--- cmd/preview.go
+++ cmd/preview.go
@@ -14,8 +14,8 @@ import (
 // previewCmd represents the preview command
 var previewCmd = &cobra.Command{
 	Use:   "preview",
-	Short: "Preview the service entries that will be created",
-	Long:  `Preview the service entries that will be created`,
+	Short: "Preview the data found in your Kubernetes cluster",
+	Long:  `This command will print out all the data it can find in your Kubernetes cluster based on the settings in the configuration file`,
 	Run:   runPreview,
 }
 
@@ -27,6 +27,8 @@ func runPreview(cmd *cobra.Command, args []string) {
 	config, err := config.New()
 	cobra.CheckErr(err)
 
+	fmt.Println("The following data was found in your Kubernetes cluster ...")
+
 	services, err2 := common.QueryForServices(config)
 	cobra.CheckErr(err2)
 
@@ -35,4 +37,6 @@ func runPreview(cmd *cobra.Command, args []string) {
 		fmt.Printf("[]\n")
 	}
 	fmt.Printf("%s\n", string(prettyJSON))
+
+	fmt.Println("\nIf you're happy with the above data you can reconcile it with OpsLevel by running:\n\n OL_APITOKEN=XXX kubectl opslevel service import\n\nOtherwise, please adjust the config file and rerun this command")
 }
