527681c9f1e1454a9fa55de7961566d92544a7a2
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-13 23:54:13 -0500

    fix bug where SAMPLES_COUNT not set

--- src/cmd/preview.go
+++ src/cmd/preview.go
@@ -15,17 +15,23 @@ import (
 // previewCmd represents the preview command
 var previewCmd = &cobra.Command{
 	Use:   "preview [SAMPLES_COUNT]",
-	Short: "Preview the data found in your Kubernetes cluster returning SAMPLES_COUNT",
+	Short: "Preview the data found in your Kubernetes cluster",
 	Long: `This command will print out all the data it can find in your Kubernetes cluster based on the settings in the configuration file.
+By default a randomly selected sample will be shown.
 If the optional argument SAMPLES_COUNT=0 this will print out everything.`,
 	Args:       cobra.MaximumNArgs(1),
-	ArgAliases: []string{"samples"},
+	ArgAliases: []string{"SAMPLES_COUNT"},
 	Run: func(cmd *cobra.Command, args []string) {
-		sampleCount := 5
-		if len(args) > 0 {
-			if parsedSamples, err := strconv.Atoi(args[0]); err == nil {
-				sampleCount = parsedSamples
+		var (
+			err         error
+			sampleCount int = 5
+		)
+		if len(args) == 1 {
+			sampleCount, err = strconv.Atoi(args[0])
+			if sampleCount < 0 {
+				err = fmt.Errorf("SAMPLES_COUNT must be >= 0, got %d", sampleCount)
 			}
+			cobra.CheckErr(err)
 		}
 
 		config, err := LoadConfig()
@@ -60,13 +66,15 @@ func PrintServices(isTextOutput bool, samples int, queue <-chan opslevel_jq_pars
 	fmt.Println(string(prettyJSON))
 
 	if isTextOutput {
-		servicesCount := len(*services)
-		if samples < servicesCount {
-			if samples == 0 {
-				samples = servicesCount
-			}
-			fmt.Printf("\nShowing %v / %v resources\n", samples, servicesCount)
+		var (
+			servicesCount int    = len(*services)
+			midText       string = "a random sample of "
+		)
+		if samples == 0 || samples >= servicesCount {
+			samples = servicesCount
+			midText = ""
 		}
+		fmt.Printf("\nShowing %s%d / %d resources\n", midText, samples, servicesCount)
 		fmt.Println("\nIf you're happy with the above data you can reconcile it with OpsLevel by running:\n\n OPSLEVEL_API_TOKEN=XXX kubectl opslevel service import\n\nOtherwise, please adjust the config file and rerun this command")
 	}
 }
