1ae8bd34025eadf5775958d9911e3eb0c1540678
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-16 19:07:16 -0500

    sync and async but not using context yet

--- src/cmd/reconcile.go
+++ src/cmd/reconcile.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"time"
 
 	opslevel_common "github.com/opslevel/opslevel-common/v2023"
@@ -28,7 +29,8 @@ var reconcileCmd = &cobra.Command{
 		common.SyncCache(client)
 		common.SyncCaches(createOpslevelClient(), resync)
 		queue := make(chan opslevel_jq_parser.ServiceRegistration, 1)
-		common.SetupControllers(config, queue, resync)
+		reconcilerCtx, _ := context.WithCancel(context.Background()) // TODO: cancel on SIGINT/SIGTERM
+		common.SetupControllers(config, queue, resync, reconcilerCtx)
 		common.ReconcileServices(client, disableServiceCreation, queue)
 		opslevel_common.Run("Controller")
 	},
