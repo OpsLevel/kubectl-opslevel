5dd7c31db0973b5466c822147aa832892882495a
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-22 12:20:22 -0500

    Fix tools not being deduplicated | Fix bug where tools logging causes nil pointer exception (#233)

--- src/common/reconciler_test.go
+++ src/common/reconciler_test.go
@@ -403,3 +403,60 @@ func Test_Reconciler_ServiceNeedsUpdate(t *testing.T) {
 		autopilot.Equals(t, test.result, reconciler.ServiceNeedsUpdate(test.input, &test.service))
 	})
 }
+
+func Test_Reconciler_HandleTools(t *testing.T) {
+	// Arrange
+	registration := opslevel_jq_parser.ServiceRegistration{
+		Aliases: []string{"a_test_service"},
+		Tools:   newToolInputs("A", "B", "C", "D", "E", "F", "G"),
+	}
+	service := opslevel.Service{
+		ServiceId: opslevel.ServiceId{
+			Id: opslevel.ID("XXX"),
+		},
+		Name: "ATestService",
+		Tools: &opslevel.ToolConnection{
+			Nodes: newTools("A", "B", "C", "D", "E"),
+		},
+	}
+	toolsCreated := []opslevel.ToolCreateInput{}
+	reconciler := common.NewServiceReconciler(&common.OpslevelClient{
+		GetServiceHandler: func(alias string) (*opslevel.Service, error) {
+			return &service, nil
+		},
+		CreateToolHandler: func(tool opslevel.ToolCreateInput) error {
+			toolsCreated = append(toolsCreated, tool)
+			return nil
+		},
+	}, false)
+	// Act
+	err := reconciler.Reconcile(registration)
+	autopilot.Ok(t, err)
+	// Assert
+	autopilot.Assert(t, len(toolsCreated) == 2 && toolsCreated[0].DisplayName == "F" &&
+		toolsCreated[1].DisplayName == "G", "expected tools created to be ['F','G']")
+}
+
+func newToolInputs(names ...string) []opslevel.ToolCreateInput {
+	inputs := make([]opslevel.ToolCreateInput, len(names))
+	for i, d := range names {
+		inputs[i] = opslevel.ToolCreateInput{
+			Category:    opslevel.ToolCategoryCode,
+			DisplayName: d,
+			Environment: opslevel.RefOf("XXX"),
+		}
+	}
+	return inputs
+}
+
+func newTools(names ...string) []opslevel.Tool {
+	tools := make([]opslevel.Tool, len(names))
+	for i, d := range names {
+		tools[i] = opslevel.Tool{
+			Category:    opslevel.ToolCategoryCode,
+			DisplayName: d,
+			Environment: "XXX",
+		}
+	}
+	return tools
+}
