332055e36df07752f4e097f777e75a1ba86319b1
Taimoor Ahmad <taimoor@opslevel.com>
2024-03-13 20:36:56 -0400

    save work

--- src/cmd/preview.go
+++ src/cmd/preview.go
@@ -2,75 +2,43 @@ package cmd
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"strconv"
-
 	"github.com/opslevel/kubectl-opslevel/common"
 	opslevel_jq_parser "github.com/opslevel/opslevel-jq-parser/v2024"
 	_ "github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"strconv"
+	"time"
 )
 
 // previewCmd represents the preview command
 var previewCmd = &cobra.Command{
-	Use:   "preview [SAMPLES_COUNT]",
-	Short: "Preview the data found in your Kubernetes cluster",
-	Long: `This command will print out all the data it can find in your Kubernetes cluster based on the settings in the configuration file.
-By default a randomly selected sample will be shown.
-If the optional argument SAMPLES_COUNT=0 this will print out everything.`,
+	Use:        "preview",
+	Short:      "[PREVIEW MODE] Create or Update service entries in OpsLevel",
+	Long:       "This is a preview of the service commands",
 	Args:       cobra.MaximumNArgs(1),
-	ArgAliases: []string{"SAMPLES_COUNT"},
+	ArgAliases: []string{"resync interval (seconds)"},
 	Run: func(cmd *cobra.Command, args []string) {
-		var (
-			err         error
-			sampleCount = 5
-		)
-		if len(args) == 1 {
-			sampleCount, _ = strconv.Atoi(args[0])
+		var err error
+		previewResync := 45
+		if len(args) > 0 {
+			previewResync, err = strconv.Atoi(args[0])
+			if err != nil {
+				panic(err)
+			}
 		}
-
+		resync := time.Second * time.Duration(previewResync)
 		config, err := LoadConfig()
 		cobra.CheckErr(err)
 
 		queue := make(chan opslevel_jq_parser.ServiceRegistration, 1)
 		ctx := common.InitSignalHandler(context.Background(), queue)
-		client := createOpslevelClient()
-		common.SyncCache(client)
-		common.SetupControllers(ctx, config, queue, 0)
-		PrintServices(IsTextOutput(), sampleCount, queue)
+		// TODO: pick up from here
+		//client := createOpslevelClient()
+		common.SetupControllers(ctx, config, queue, resync)
+		common.ReconcileServices(client, disableServiceCreation, queue)
 	},
 }
 
 func init() {
 	serviceCmd.AddCommand(previewCmd)
-}
-
-func PrintServices(isTextOutput bool, samples int, queue <-chan opslevel_jq_parser.ServiceRegistration) {
-	services := common.AggregateServices(queue)
-	// Deduplicate ServiceRegistrations
-
-	// Sample the data
-	sampled := common.GetSample[opslevel_jq_parser.ServiceRegistration](samples, *services)
-
-	// Print
-	if isTextOutput {
-		fmt.Print("The following data was found in your Kubernetes cluster ...\n\n")
-	}
-
-	prettyJSON, err := json.MarshalIndent(sampled, "", "    ")
-	cobra.CheckErr(err)
-	fmt.Println(string(prettyJSON))
-	fmt.Println()
-
-	if isTextOutput {
-		servicesCount := len(*services)
-		if samples <= 0 || samples >= servicesCount {
-			fmt.Printf("This is the full list of %d services detected in your cluster.\n", servicesCount)
-		} else {
-			fmt.Printf("This is randomly selected list of %d / %d services detected in your cluster.\n", samples, servicesCount)
-			fmt.Println("If you want to see the full list of services detected, pass SAMPLES_COUNT=0.")
-		}
-		fmt.Println("\nIf you're happy with the above data you can reconcile it with OpsLevel by running:\n\n OPSLEVEL_API_TOKEN=XXX kubectl opslevel service import\n\nOtherwise, please adjust the config file and rerun this command")
-	}
 }
