72ee07a75997ef2de3f6f1f20aa0b57c1217ba4a
David Bloss <david@opslevel.com>
2024-03-14 14:32:54 -0500

    move config examples to yaml files, configs updated with extra fields

--- src/common/config.go
+++ src/common/config.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	_ "embed"
+
 	"github.com/creasty/defaults"
 	opslevel_jq_parser "github.com/opslevel/opslevel-jq-parser/v2024"
 	opslevel_k8s_controller "github.com/opslevel/opslevel-k8s-controller/v2024"
@@ -23,79 +25,11 @@ type Config struct {
 
 var ConfigCurrentVersion = "1.3.0"
 
-var ConfigSimple = `#Simple Opslevel CLI Config
-version: "1.3.0"
-service:
-  import:
-    - selector: # This limits what data we look at in Kubernetes
-        apiVersion: "apps/v1" # only supports resources found in 'kubectl api-resources --verbs="get,list"'
-        kind: Deployment
-        excludes: # filters out resources if any expression returns truthy
-          - .metadata.namespace == "kube-system"
-          - .metadata.annotations."opslevel.com/ignore"
-      opslevel: # This is how you map your kubernetes data to opslevel service
-        name: .metadata.name
-        owner: .metadata.namespace
-        aliases: # This are how we identify the services again during reconciliation - please make sure they are very unique
-          - '"k8s:\(.metadata.name)-\(.metadata.namespace)"'
-        properties:
-          # assign the value of a property named 'prop_string' onto this service
-          prop_string: .metadata.annotations.prop_string
-        tags:
-          assign: # tag with the same key name but with a different value will be updated on the service
-            - '{"imported": "kubectl-opslevel"}'
-            - .metadata.labels
-          create: # tag with the same key name but with a different value with be added to the service
-            - '{"environment": .spec.template.metadata.labels.environment}'
-`
+//go:embed configs/config_sample.yaml
+var ConfigSample string
 
-var ConfigSample = `#Sample Opslevel CLI Config
-version: "1.3.0"
-service:
-  import:
-    - selector: # This limits what data we look at in Kubernetes
-        apiVersion: "apps/v1" # only supports resources found in 'kubectl api-resources --verbs="get,list"'
-        kind: Deployment
-        excludes: # filters out resources if any expression returns truthy
-          - .metadata.namespace == "kube-system"
-          - .metadata.annotations."opslevel.com/ignore"
-      opslevel: # This is how you map your kubernetes data to opslevel service
-        name: .metadata.name
-        description: .metadata.annotations."opslevel.com/description"
-        owner: .metadata.annotations."opslevel.com/owner"
-        lifecycle: .metadata.annotations."opslevel.com/lifecycle"
-        tier: .metadata.annotations."opslevel.com/tier"
-        product: .metadata.annotations."opslevel.com/product"
-        language: .metadata.annotations."opslevel.com/language"
-        framework: .metadata.annotations."opslevel.com/framework"
-        #system: .metadata.annotations."opslevel.com/system"
-        aliases: # This are how we identify the services again during reconciliation - please make sure they are very unique
-          - '"k8s:\(.metadata.name)-\(.metadata.namespace)"'
-          - '"\(.metadata.namespace)-\(.metadata.name)"'
-        properties:
-          # assign the value of a property named 'prop_object' (is a JSON object) onto this service
-          prop_object: .metadata.annotations.prop_object
-        tags:
-          assign: # tag with the same key name but with a different value will be updated on the service
-            - '{"imported": "kubectl-opslevel"}'
-            # find annotations with format: opslevel.com/tags.<key name>: <value>
-            - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tags"))) | map({(.key | split(".")[2]): .value})'
-            - .metadata.labels
-          create: # tag with the same key name but with a different value with be added to the service
-            - '{"environment": .spec.template.metadata.labels.environment}'
-        tools:
-          - '{"category": "other", "environment": "production", "displayName": "my-cool-tool", "url": .metadata.annotations."example.com/my-cool-tool"} | if .url then . else empty end'
-          # find annotations with format: opslevel.com/tools.<category>.<displayname>: <url>
-          - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tools"))) | map({"category": .key | split(".")[2], "displayName": .key | split(".")[3], "url": .value})'
-          # OR find annotations with format: opslevel.com/tools.<category>.<environment>.<displayname>: <url>
-          # - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tools"))) | map({"category": .key | split(".")[2], "environment": .key | split(".")[3], "displayName": .key | split(".")[4], "url": .value})'
-        repositories: # attach repositories to the service using the opslevel repo alias - IE github.com:hashicorp/vault
-          - '{"name": "My Cool Repo", "directory": "", "repo": .metadata.annotations.repo} | if .repo then . else empty end'
-          # if just the alias is returned as a single string we'll build the name for you and set the directory to "/"
-          - .metadata.annotations.repo
-          # find annotations with format: opslevel.com/repo.<displayname>.<repo.subpath.dots.turned.to.forwardslash>: <opslevel repo alias>
-          - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/repo"))) | map({"name": .key | split(".")[2], "directory": .key | split(".")[3:] | join("/"), "repo": .value})'
-`
+//go:embed configs/config_simple.yaml
+var ConfigSimple string
 
 func ParseConfig(data string) (*Config, error) {
 	var output Config
